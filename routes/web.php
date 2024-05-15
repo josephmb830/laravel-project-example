3b776452b8b56a68860eca0c462c9bed463fecd1
Joseph Morán <josephmoran@duck.com>
2024-05-14 21:47:08 -0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,26 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+// Route::get    | Consultar
+// Route::post   | Guardar 
+// Route::delete | Eliminar
+// Route::put    | Actualizar
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    //consulta a base de datos
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
 });
